8eeeb4ae6c8cbecb70c982d54de2611c4fddc7b4
David.Gao <david.alpha.fox@gmail.com>
2015-09-16 18:35:24 +0800

    delete link

--- dist/js/app.js
+++ dist/js/app.js
@@ -45,14 +45,10 @@
       url: 'http://my.oschina.net/u/236698'
     }, {
       id: 3,
-      title: '我的甜点小屋',
-      url: 'http://shop121709433.taobao.com/?spm=a1z10.1-c.0.0.7I96nr'
-    }, {
-      id: 4,
       title: '关于我',
       url: 'http://cn.linkedin.com/in/gaotianpo'
     }, {
-      id: 5,
+      id: 4,
       title: 'VPS提供商',
       url: 'http://www.vultr.com/?ref=6833358'
     }
